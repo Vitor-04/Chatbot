014660cbefde4ace4e584cda17e82c193f74dcff
Vitor-04 <71941047+Vitor-04@users.noreply.github.com>
2020-11-01 16:09:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def assistente():
 			break
 
 
-	print("="* len(apresentacao)) #verifica o tamanho do nome inserido
+	print("="* len(apresentacao))
 	print("Aguardando solicitação...")
 
 	while True:
@@ -50,19 +50,74 @@ def assistente():
 
 			while True:
 				try:
-
-					
 					audio = rec.listen(s)
 					entrada = rec.recognize_google(audio, language="pt")
 					print("{}: {}".format(user_name, entrada))
 
-					reposta = conversas[entrada]
+				 # Abre links no navegador
+					if "Abrir" in entrada:
+						reposta = abrir(entrada)
 
-					print("Assistente: {}".format(reposta))
-					sai_som("{}".format(reposta))
+					if "notícias" in entrada:
+						reposta = noticia(entrada)
+
+				# Operações matemáticas
+					elif "Quanto é" in entrada:
+
+						entrada = entrada.replace("Quanto é", "")
+						reposta = calcula(entrada)
+				# Pede temperatura
+					elif "temperatura" in entrada:
+
+						lista_tempo = temperatura()
+						temp = lista_tempo[0]
+						temp_max = lista_tempo[1]
+						temp_min = lista_tempo[2]
+
+						reposta = "A temperatura de hoje é {:.2f}º. Temos uma máxima de {:.2f}º e uma minima de {:.2f}º".format(temp, temp_max, temp_min)
+
+				# Informações da cidade
+					elif "informações" in entrada and "cidade" in entrada:
+
+						reposta = "Mostrando informações da cidade"
+					else:
+						reposta = conversas[entrada]
+
+					if reposta == "Mostrando informações da cidade":
+						#mostra informações da cidade
+
+						lista_infos = clima_tempo()
+						longitude = lista_infos[0]
+						latitude = lista_infos[1]
+						temp = lista_infos[2]
+						pressao_atm = lista_infos[3]
+						humidade = lista_infos[4]
+						temp_max = lista_infos[5]
+						temp_min = lista_infos[6]
+						v_speed = lista_infos[7]
+						v_direc = lista_infos[8]
+						nebulosidade = lista_infos[9]
+						id_da_cidade = lista_infos[10]
+
+						print("Assistente:")
+						print("Mostrando informações de {}\n\n".format(cidade))
+						sai_som("Mostrando informações de {}".format(cidade))
+						print("Longitude: {}, Latitude: {}\nId: {}\n".format(longitude, latitude, id_da_cidade))
+						print("Temperatura: {:.2f}º".format(temp))
+						print("Temperatura máxima: {:.2f}º".format(temp_max))
+						print("Temperatura minima: {:.2f}º".format(temp_min))
+						print("Humidade: {}".format(humidade))
+						print("Nebulosidade: {}".format(nebulosidade))
+						print("Velocidade do vento: {}m/s\nDireção do vento: {}".format(v_speed,v_direc))
+
+					else:
+						print("Assistente: {}".format(reposta))
+						sai_som("{}".format(reposta))
 
 				except sr.UnknownValueError:
 					sai_som(resposta_erro_aleatoria)
+				except KeyError:
+					pass
 
 
 if __name__ == '__main__':
