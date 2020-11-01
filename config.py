a0bfd12a74b8a74646a80a63c79f90d5ad5e0d18
Vitor-04 <71941047+Vitor-04@users.noreply.github.com>
2020-11-01 16:08:38 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import requests as rq
 import webbrowser as web 
 
-
+cidade = 'curitiba'
 caminho_navegador = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
 
 def intro():
@@ -12,17 +12,24 @@ def intro():
 lista_erros = [
 		"Não entendi nada",
 		"Desculpe, não entendi",
-		"Repita novamente por favor",
-		"não entendi"
+		"Repita novamente por favor"
 ]
 
 conversas = {
-	"Olá": "oi, tudo bem?",
+	"Olá": "olá, tudo bem?",
+	"olá": "oi, tudo bem?",
+	"ola": "oi, tudo bem?",
 	"Oi": "oi, tudo bem?",
-	"sim e você": "Estou bem, obrigada por perguntar",
+	"oi": "oi, tudo bem?",
+	"sim e você": "Estou bem, obrigado!",
+	"sim e voce": "Estou bem obrigado!",
+	"sim, e você": "Estou bem obrigada por perguntar",
+	"sim, e você?": "Estou bem obrigada por perguntar",
+}
 
-  "Alguma sugestão?":"Curso de inglês ou algo de informática",
-  "Algum outro curso?":"Curso de paraquedismo"
+comandos = {
+	"desligar": "desligando",
+	"reiniciar": "reiniciando"
 }
 
 
@@ -64,7 +71,7 @@ def  verifica_nome_exist(nome):
 	vazio.writelines(conteudo)
 	vazio.close()
 
-	return "Olá {}, é a primeira vez que nos falamos".format(nome)
+	return "Oi {} é a primeira vez que nos falamos".format(nome)
 
 
 def name_list():
@@ -77,15 +84,103 @@ def name_list():
 		nomes.close()
 
 
-def abrir(entrada):
+def calcula(entrada):
+	if "mais" in entrada or "+" in entrada:
+		#soma
+		entradas_recebidas = entrada.split(" ")
+		resultado = int(entradas_recebidas[1]) + int(entradas_recebidas[3])
+
+	elif "menos" in entrada or "-" in entrada:
+		#subtração
+
+		entradas_recebidas = entrada.split(" ")
+		resultado = int(entradas_recebidas[1]) - int(entradas_recebidas[3])
+
+	elif "vezes" in entrada or "x" in entrada:
+		#multiplicação
+
+		entradas_recebidas = entrada.split(" ")
+		resultado = round(float(entradas_recebidas[1]) * float(entradas_recebidas[3]), 2)
+
+	elif "dividido" in entrada or "/" in entrada:
+		#divisão
+
+		entradas_recebidas = entrada.split(" ")
+		resultado = round(float(entradas_recebidas[1]) / float(entradas_recebidas[4]), 2)
+
+	else:
+
+		resultado = "Operação não encontrada"
+
+
+	return resultado
+
+
+
+def clima_tempo():	
+	endereco_api = "http://api.openweathermap.org/data/2.5/weather?appid=9e1280f88eef9db700e867bb898fd3ec&q="
+	url = endereco_api + cidade
+
+	infos = rq.get(url).json()
+
+
+	# Coord
+	longitude = infos['coord']['lon']
+	latitude = infos['coord']['lat']
+	# main
+	temp = infos['main']['temp'] - 273.15 # Kelvin para Celsius
+	pressao_atm = infos['main']['pressure'] / 1013.25 #Libras para ATM
+	humidade = infos['main']['humidity'] # Recebe em porcentagem
+	temp_max= infos['main']['temp_max'] - 273.15 # Kelvin para Celsius
+	temp_min = infos['main']['temp_min'] - 273.15 # Kelvin para Celsius
+
+	#vento
+	v_speed = infos['wind']['speed'] # km/ h
+	v_direc = infos['wind']['deg'] #Recebe em graus
+
+	#clouds / nuvens
+
+	nebulosidade = infos['clouds']['all']
+
+	#id
+	id_da_cidade = infos['id']
+
+	# 11
+	return [longitude, latitude, 
+		temp, pressao_atm, humidade, 
+		temp_max, temp_min, v_speed, 
+		v_direc, nebulosidade, id_da_cidade]
+
+
+def temperatura():
+	temp_atual = clima_tempo()[2]
+	temp_max = clima_tempo()[5]
+	temp_min = clima_tempo()[6]
+	
+	return [temp_atual, temp_max, temp_min]
+
+
+
+def abrir(fala):
 	try:
-		if "google" in entrada:
+		if "Google" in fala:
 			web.get(caminho_navegador).open("google.com.br/")
 			return "abrindo google"
-		elif "facebook" in entrada:
+		elif "Facebook" in fala:
 			web.get(caminho_navegador).open("facebook.com.br/")
 			return "abrindo facebook"
 		else:
 			return "site não cadastrado para aberturas"
+	except:
+		return "houve um erro"
+
+def noticia(fala):
+	try:
+		if "politica" in fala:
+			web.get(caminho_navegador).open("https://www.google.com/search?q=noticias+politica")
+			return"Mostrando resultados da pesquisa por {}".format(fala)
+		else:
+			web.get(caminho_navegador).open("https://www.google.com/search?q={}".format(fala))
+			return"Mostrando resultados da pesquisa por {}".format(fala)
 	except:
 		return "houve um erro"
